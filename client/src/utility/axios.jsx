86d8ac321671861abea9895dda2cf284cd451236
Bereket18 <bereketadamsseged@gmail.com>
2023-09-03 16:08:41 +0300

    test

--- client/src/utility/axios.jsx
+++ client/src/utility/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://sleepy-newt-tuxedo.cyclic.cloud",
+	baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
 	withCredentials: true,
 });
 
